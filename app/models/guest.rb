8db9cc9044e2b969810d476570986f4631305cee
unknown <cadaciojerico@gmail.com>
2025-11-29 03:09:41 +0800

    Add validations for guest and reservation models.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,13 @@
 class Guest < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  
+  validates :email, :first_name, :last_name, :phone_numbers, 
+            presence: true
+
+  validates :phone_numbers,
+            length: { minimum: 1 }
+
+  validates :email,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,55 @@
+
 class Reservation < ApplicationRecord
   belongs_to :guest
+
+  ALLOWED_STATUSES = %w[
+    accepted
+    cancelled
+    pending
+  ].freeze
+
+  ALLOWED_CURRENCIES = %w[
+    AUD
+    USD
+  ].freeze
+
+  validates :adults,
+            :children,
+            :currency,
+            :end_date,
+            :guest_count,
+            :infants,
+            :nights,
+            :payout_price,
+            :security_price,
+            :start_date,
+            :status,
+            :total_price,
+            presence: true
+
+  # Adults must be at least 1
+  validates :adults,
+            :guest_count,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1
+            }
+
+  # Included nights >= 0 in case same day checkout will be supported in the future.
+  validates :children,
+            :infants,
+            :nights,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates :status,
+            inclusion: {
+              in: ALLOWED_STATUSES,
+              message: "must be one of: #{ALLOWED_STATUSES.join(', ')}"
+            }
+
+   validates :currency,
+            inclusion: {
+              in: ALLOWED_CURRENCIES,
+              message: "must be one of: #{ALLOWED_CURRENCIES.join(', ')}"
+            }
 end
